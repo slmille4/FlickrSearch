c4eb2b9eebd8a76003700163ea0361db587596ec
Steve Miller <slmille4@gmail.com>
2025-01-15 13:06:04 -0500

    Initial Commit

---
+++ FlickrSearch/FlickrSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearchApp.swift
+//  FlickrSearch
+//
+//  Created by Steve on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FlickrSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
